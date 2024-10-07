3d7875e1ef7e65280dee600abee7f27ae4590189
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-10-07 16:03:01 +0700

    btvn B25 Ex04

--- Day8/BTVN/ex04.js
+++ Day8/BTVN/ex04.js
@@ -4,4 +4,14 @@ Viết hàm `countDownToNewYear` để đếm số ngày còn lại đến tết
 
 */
 
+console.log("Còn", countDownToNewYear(), "ngày là tới tết dương lịch" );
+function countDownToNewYear() {
+
+	let today = new Date(); // Lấy thời gian hiện tại
+	let newYear = new Date( today.getFullYear() + 1, 0 ) // Lấy thời gian tương lai là số năm hiện tại + 1, số tháng từ index 0
+	let timeRemaining = newYear - today // mili giây
+
+	return Math.floor( timeRemaining / ( 1000 * 60 * 60 * 24 ));
+}
+
 
