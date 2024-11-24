4c7a682dfb9994e904435926964db855cd6407c1
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-11-24 14:51:51 +0700

    K10 B39 React 02 off line nguyen

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/Header";
+
+/**
+ *
+ ** Bài 1: Tạo 1 nút bấm toogle product list để ẩn hiện danh sách sản phẩm.
+ ** Bài 2: Tạo nút bấm "See more" ở cuối danh sách sản phẩm, mỗi lần ấn sẽ hiển thị thêm 10 sản phẩm.
+ ** Bài 3: Trong component Header tạo nút bấm "handleChangeTheme" để chuyển đổi giữa DarkMode và LightMode
+ */
+
+function App() {
+	return (
+		<>
+			<Header />
+			<h1>Hello</h1>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
