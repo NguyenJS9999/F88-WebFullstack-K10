2da2bd44cefb43f58a76431cf762b3c64769c310
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-12-04 21:20:52 +0700

    B44 React 07

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import instance, { getAll, removeById } from "./axios";
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
-import CategoryPage from "./pages/CategoryPage";
-import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import ProductDetail from "./pages/ProductDetail";
-import ServicesPage from "./pages/ServicesPage";
+import Footer from "./layout/footer/Footer";
+import Header from "./layout/header/Header";
 import Dashboard from "./pages/admin/Dashboard";
 import ProductForm from "./pages/admin/ProductForm";
 import ProductTable from "./pages/admin/ProductTable";
+import CategoryPage from "./pages/CategoryPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProductDetail from "./pages/ProductDetail";
+import RegisterPage from "./pages/RegisterPage";
+import ServicesPage from "./pages/ServicesPage";
+import { getAll, removeById } from "./services/crudServices";
+import LayoutAdmin from "./layout/LayoutAdmin";
 
 const App = () => {
 	const [products, setProducts] = useState([]);
@@ -44,11 +47,15 @@ const App = () => {
 				<Route path="/categories" element={<CategoryPage />} />
 				<Route path="/products/:id" element={<ProductDetail />} />
 
-				<Route path="/admin" element={<Dashboard />}>
+				<Route path="/admin" element={<LayoutAdmin />}>
+					<Route index element={<Dashboard />} />
 					<Route path="products" element={<ProductTable products={products} onRemove={handleRemoveProduct} />} />
 					<Route path="products/add" element={<ProductForm />} />
 					<Route path="products/update/:id" element={<ProductForm />} />
 				</Route>
+
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/login" element={<LoginPage />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 			<Footer />
