09042eb5bbaa3ae475b9f570add0d1f3358dbd51
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-12-06 14:28:26 +0700

    F8 K10 B44 react07

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import RegisterPage from "./pages/RegisterPage";
 import ServicesPage from "./pages/ServicesPage";
 import { getAll, removeById } from "./services/crudServices";
 import LayoutAdmin from "./layout/LayoutAdmin";
+import ProtectedRoute from "./layout/ProtectedRoute";
+import SuperAdmin from "./pages/admin/SuperAdmin";
 
 const App = () => {
 	const [products, setProducts] = useState([]);
@@ -53,6 +55,20 @@ const App = () => {
 					<Route path="products/add" element={<ProductForm />} />
 					<Route path="products/update/:id" element={<ProductForm />} />
 				</Route>
+
+				{/* superAdmin */}
+				{/* <Route path="/super-admin" element={<ProtectedRoute />}>
+					<Route index element={<SuperAdmin />} />
+				</Route> */}
+
+				<Route
+					path="/super-admin"
+					element={
+						<ProtectedRoute role="superAdmin">
+							<SuperAdmin />
+						</ProtectedRoute>
+					}
+				/>
 
 				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/login" element={<LoginPage />} />
