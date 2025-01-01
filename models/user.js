81ebdc7fbfc704a3d80a2c154d516f764511f63b
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2025-01-01 18:20:21 +0700

    b54_backend_b2 follow teacher

--- models/user.js
+++ models/user.js
@@ -1,23 +1,26 @@
-import mongoose, { version } from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    role: {
-      type: String,
-      default: "member",
-      enum: ["member", "admin", "superAdmin"],
-    },
-  },
-  { versionKey: false, timestamps: true }
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			default: "member",
+			enum: ["member", "admin", "superAdmin"],
+		},
+	},
+	{
+		versionKey: false,
+		timestamps: true,
+	}
 );
 
 export default mongoose.model("User", userSchema);
