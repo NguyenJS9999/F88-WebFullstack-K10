f54b4e75550a15e25499932939fae7b5ac675fe6
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2025-02-07 14:42:24 +0700

    F8 K10 B62 Tuwj code noosi UD variants

--- B63_node10-VariantDb/src/controllers/productVariantControllers.js
+++ B63_node10-VariantDb/src/controllers/productVariantControllers.js
@@ -32,3 +32,65 @@ export const getVariantsByProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+
+// 3 Lấy chi tiết một biến thể theo ID
+export const getVariantById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const variant = await ProductVariant.findById(id);
+    if (!variant) {
+      return res.status(404).json({ message: "Variant not found" });
+    }
+    res.json({
+      message: "Get variant successfully",
+      variant,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Cập nhật thông tin biến thể
+export const updateVariantProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { size, color, price, stock } = req.body;
+
+    const updatedVariant = await ProductVariant.findByIdAndUpdate(
+      id,
+      { size, color, price, stock },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedVariant) {
+      return res.status(404).json({ message: "Variant not found" });
+    }
+
+    res.json({
+      message: "Update variant successfully",
+      updatedVariant,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Xóa một biến thể sản phẩm
+export const deleteVariantProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedVariant = await ProductVariant.findByIdAndDelete(id);
+
+    if (!deletedVariant) {
+      return res.status(404).json({ message: "Variant not found" });
+    }
+
+    res.json({
+      message: "Delete variant successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
